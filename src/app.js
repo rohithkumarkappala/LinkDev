5c73f56034417d0e94fe5be9c532031f04ff58d9
rohithkumarkappala <rohithkumark2005@gmail.com>
2025-05-11 21:29:23 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const app = express();
-app.use("/", (req, res) => {
-    res.send("Hello World");
+
+app.get("/user",(req,res,next)=>{
+    console.log("Response 1!!")
+    res.send("Hello from GET API call")
+    next();
+    
+},(req,res,next)=>{
+    console.log("Response 2!!")
+    // res.send("Hello from GET API call")
+    next();
+},(req,res,next)=>{
+    console.log("Response 3!!")
+    // res.send("Hello from GET API call")
+    next();
+},(req,res,next)=>{
+    console.log("Response 4!!")
+    // res.send("Hello from GET API call")
+    next();
+},(req,res,next)=>{
+    console.log("Response 5!!")
+    // res.send("Hello from GET API call")
+    next();
 }
 );
-console.log("starting app.js");
+
+app.post("/user",(req,res)=>{
+    res.send("Data Successfully stored in DataBase")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Data Successfully deleted from DataBase")
+});
+
+app.use("/test", (req,res)=>{
+    res.send("Hello from /hello");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
